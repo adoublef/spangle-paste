47df2b4ef052d8b5fd97134102b48c3fa16d65e0
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-08-27 19:44:04 +0100

    add service layer

---
+++ internal/http/service.go
@@ -0,0 +1,38 @@
+package http
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+var _ http.Handler = (*Service)(nil)
+
+type Service struct {
+	m *chi.Mux
+}
+
+// ServeHTTP implements http.Handler.
+func (s *Service) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	s.m.ServeHTTP(w, r)
+}
+
+func NewService() *Service {
+	s := &Service{
+		m: chi.NewMux(),
+	}
+	s.routes()
+	return s
+}
+
+func (s *Service) routes() {
+	s.m.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		name := r.URL.Query().Get("name")
+		if name == "" {
+			name = "World"
+		}
+
+		fmt.Fprintf(w, "Hello, %s!", name)
+	})
+}
---
+++ internal/http/service_test.go
@@ -0,0 +1,32 @@
+package http_test
+
+import (
+	"io"
+	"net/http/httptest"
+	"testing"
+
+	api "github.com/adoublef/spangle-paste/internal/http"
+	"github.com/stretchr/testify/require"
+)
+
+func TestService(t *testing.T) {
+	srv := newTestService(t)
+
+	t.Cleanup(func() { srv.Close() })
+
+	// TODO -- make http request
+	t.Run("returns `Hello, Foo!`", func(t *testing.T) {
+		resp, err := srv.Client().Get(srv.URL+"?name=Foo")
+		require.NoError(t, err, "failed to make get request")
+
+		p, err := io.ReadAll(resp.Body)
+		require.NoError(t, err, "failed to read body")
+
+		require.Equal(t, "Hello, Foo!", string(p))
+	})
+}
+
+func newTestService(t *testing.T) *httptest.Server {
+	h := api.NewService()
+	return httptest.NewServer(h)
+}
