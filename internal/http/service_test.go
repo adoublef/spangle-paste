cf35ba3906143f7b307370516374e4a1283a2024
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-08-28 01:15:28 +0100

    convert to italian

--- internal/http/service_test.go
+++ internal/http/service_test.go
@@ -15,14 +15,14 @@ func TestService(t *testing.T) {
 	t.Cleanup(func() { srv.Close() })
 
 	// TODO -- make http request
-	t.Run("returns `Hello, Foo!`", func(t *testing.T) {
+	t.Run("returns `Ciao, Foo!`", func(t *testing.T) {
 		resp, err := srv.Client().Get(srv.URL+"?name=Foo")
 		require.NoError(t, err, "failed to make get request")
 
 		p, err := io.ReadAll(resp.Body)
 		require.NoError(t, err, "failed to read body")
 
-		require.Equal(t, "Hello, Foo!", string(p))
+		require.Equal(t, "Ciao, Foo!", string(p))
 	})
 }
 
